db4549126d81517e0cca2b45d4b7a607f240ac6f
pecee06 <priyanshu.choudhury2406@outlook.com>
2024-02-21 22:08:36 +0530

    create user & video models

---
+++ src/models/user.model.js
@@ -0,0 +1,82 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";    // for hashing password
+import jwt from "jsonwebtoken"  // for transmitting json data securely over network
+
+import { SALTING_ROUNDS } from "../constants.js";
+
+const userSchema = new mongoose.Schema({
+    watchHistory: [{
+        type: mongoose.Schema.Types.ObjectId,   // custom schema type
+        ref: "Video"
+    }],
+    username: {
+        type: String,
+        unique: [true,"This username already exists"],
+        required: [true,"Username is a required field"],
+        index: true // makes the field searchable
+    },
+    email: {
+        type: String,
+        unique: [true,"This email already exists"],
+        required: [true,"Email is a required field"],
+        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+        index: true
+    },
+    fullName: {
+        type: String,
+        default: "Guest User"
+    },
+    avatar: String,
+    coverImage: String,
+    password: {
+        type: String,
+        required: [true,"Password is a required field"]
+    },
+    refreshToken: {
+        type: String
+    }
+}, {timestamps: true});
+
+// Middlewares
+
+userSchema.pre("save", async function(next){
+    if (!this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password, SALTING_ROUNDS);
+    next(); // Simce this hook works as a middleware, thus calling next is important for passing flag to next middleware or request handler
+});
+// This hook will get executed just before saving the document into DB
+
+// Making custom hooks
+
+userSchema.methods.checkPassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    // Returns signature after accepting payload & token-secret
+    let payload = {
+        id: this._id,   // generated automatically by mongodb
+        email: this.email,
+        username: this.username
+    };
+
+    return jwt.sign(
+        payload,
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    let payload = {
+        id: this._id
+    };
+
+    return jwt.sign(
+        payload,
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+
+export const User = mongoose.model("User", userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema({
+    videoFile: {
+        type: String,
+        required: [true, "Video File is required"],
+        unique: [true, "Can't upload duplicate video"]
+    },
+    thumbnail: {
+        type: String,
+        required: [true, "Thumbnail is required"]
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title: {
+        type: String,
+        required: [true, "Title of video is required"],
+        index: true
+    },
+    description: {
+        type: String,
+        required: [true, "Description of video is required"]
+    },
+    duration: {
+        type: Number,
+        required: [true, "Video duration is required"]
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    }
+}, {timestamps: true});
+
+// Middleware
+videoSchema.plugin(mongooseAggregatePaginate);  // Allows aggregate queries (min, max, avg, ...)
+
+export const Video = mongoose.model("Video",videoSchema);
